03b379934d5a4f3e5263f77e7dfa7e3ffdd874a8
Ashu Austin Sah <AshuAustin.Sah@ul.com>
2021-02-03 10:15:29 +0100

    Add some UT

--- src/test/java/com/airwallex/rpc/functions/AddTest.java
+++ src/test/java/com/airwallex/rpc/functions/AddTest.java
@@ -1,10 +1,8 @@
 package com.airwallex.rpc.functions;
 
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
-import org.junit.jupiter.params.provider.ValueSource;
 
 import java.math.BigDecimal;
 import java.util.stream.Stream;
@@ -12,21 +10,21 @@ import java.util.stream.Stream;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.params.provider.Arguments.arguments;
 
-public class SquareRootTest {
+public class AddTest {
 
-    private SquareRoot squareRoot = new SquareRoot();
+    private Add add = new Add();
 
     static Stream<Arguments> getNumbers(){
         return Stream.of(
-                arguments(new BigDecimal(4), new BigDecimal(2)),
-                arguments(new BigDecimal(64), new BigDecimal(8)));
+                arguments(new BigDecimal(4), new BigDecimal(2), new BigDecimal(6)),
+                arguments(new BigDecimal(64), new BigDecimal(8), new BigDecimal(72))
+        );
 
     }
 
     @ParameterizedTest
     @MethodSource("getNumbers")
-    void evaluateSquareRootTest(BigDecimal p1, BigDecimal expectedSqrt) {
-        BigDecimal expectedResult = new BigDecimal(2);
-        assertEquals(expectedSqrt, squareRoot.evaluateUnaryFunction(p1));
+    void evaluateSquareRootTest(BigDecimal p1, BigDecimal p2, BigDecimal expectedResult) {
+        assertEquals(expectedResult, add.evaluateBinaryFunction(p1, p2));
     }
 }
